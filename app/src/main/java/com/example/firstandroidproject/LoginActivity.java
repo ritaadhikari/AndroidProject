9670a17ecbf5b33287770326b1c0503e1b37f9b5
Rita adhikari <ritaadhikari936@gmail.com>
2023-01-04 21:25:26 +0545

    Notification

--- app/src/main/java/com/example/firstandroidproject/LoginActivity.java
+++ app/src/main/java/com/example/firstandroidproject/LoginActivity.java
@@ -1,15 +1,22 @@
 package com.example.firstandroidproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
 public class LoginActivity extends AppCompatActivity {
+    String TAG = "Firebase";
     private Button btnLogin,btnSignUp;
     private String username, password;
     private EditText etUsername, etPassword;
@@ -17,6 +24,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initFirebase();
         btnLogin = findViewById(R.id.btnLogin);
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
@@ -45,6 +54,28 @@ public class LoginActivity extends AppCompatActivity {
 
 
     }
+
+    private void initFirebase() {
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+                        String msg = String.format(R.string.msg_token_fmt+":"+token);
+                        Log.d(TAG," Firebase Token==>" +token);
+                        Toast.makeText(LoginActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
     //login input field validation
         private boolean loginValidation() {
             username = etUsername.getText().toString();
